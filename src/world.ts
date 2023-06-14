8bafca53623c72e63867744eeb8cb6289c035e98
yayaya012 <chart5@outlook.jp>
2023-06-15 01:23:33 +0900

    husky_test

--- src/world.ts
+++ src/world.ts
@@ -1,24 +1,13 @@
 export default class World {
-    message: string;
+  message: string
 
-    constructor(message: string) {
-        this.message = message
+  constructor(message: string) {
+    this.message = message
+  }
+
+  public sayHello(elem: HTMLElement | null): void {
+    if (elem) {
+      elem.innerText = this.message
     }
-
-    public sayHello(elem: HTMLElement | null): void {
-        if (elem) {
-            elem.innerText = this.message
-        }
-
-
-
-
-
-
-    }
-
-
-
-
-
+  }
 }
